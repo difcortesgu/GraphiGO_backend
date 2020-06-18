8614332ac5a5dfc2459acc056269f12aa11ab641
Farid <difcortesgu@unal.edu.co>
2020-06-17 19:23:58 -0500

    :rotating_light: small change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,6 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
-        staticFiles.location("/public");
-
 
         get("/hello", new Route() {
             public Object handle(Request req, Response res) throws Exception {
